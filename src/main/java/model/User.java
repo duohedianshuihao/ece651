8df8645fcee73c6673bb2bd499fbf487b4435e0f
Hewei Xu <h268xu@uwaterloo.ca>
2017-09-25 17:08:50 -0400

    use table attributes

---
+++ src/main/java/model/User.java
@@ -0,0 +1,34 @@
+package model;
+
+
+import com.amazonaws.services.dynamodbv2.datamodeling.*;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Data
+@DynamoDBTable(tableName = "SharkJobUser")
+public class User {
+
+    @DynamoDBHashKey
+    private String email;
+
+    @DynamoDBRangeKey
+    private String userType;
+
+    @DynamoDBVersionAttribute
+    private long version;
+
+    @DynamoDBAttribute(attributeName = "userName")
+    private String userName;
+
+    @DynamoDBAttribute(attributeName = "skills")
+    private String skills;
+
+    @DynamoDBAttribute(attributeName = "password")
+    private String password;
+
+}
