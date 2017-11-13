cecb3fedaa02948607ba88a8180e870ebe567335
Meng Pang <meng.pang@mail.mcgill.ca>
2017-11-13 14:15:15 -0500

    Modify Message Config file

---
+++ src/main/java/com/sharkjob/config/WebSocketConfig.java
@@ -0,0 +1,16 @@
+package com.sharkjob.config;
+
+import com.sharkjob.Dao.UserDao;
+import com.sharkjob.message.WebSocketMessage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public WebSocketMessage webSocket(){
+        WebSocketMessage webSocketMessage = new WebSocketMessage();
+        return webSocketMessage;
+    }
+}
