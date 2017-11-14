01035e26bb2926dc47d8ddbc619aaa5c75c1d0fe
linliulynn <linliulynn@gmail.com>
2017-11-14 12:00:56 -0500

    add test for indexController

---
+++ src/test/java/com/sharkjob/controller/PagesControllerTest.java
@@ -0,0 +1,57 @@
+package com.sharkjob.controller;
+
+import lombok.val;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PagesControllerTest {
+    @InjectMocks
+    private PagesController pagesController;
+    private String index;
+
+    @Before
+    public void setUp() {
+        index = "index";
+    }
+
+    @Test
+    public void valid_getSignupPage_successfully() {
+        val expected = index;
+        val actual = pagesController.getSignupPage();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void valid_getLoginPage_successfully() {
+        val expected = index;
+        val actual = pagesController.getLoginPage();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void valid_getJobInfoPage_successfully() {
+        val expected = index;
+        val actual = pagesController.getJobInfoPage();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void valid_getUserprofile_successfully() {
+        val expected = index;
+        val actual = pagesController.getUserprofile();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void valid_getDetailPage_successfully() {
+        val expected = index;
+        val actual = pagesController.getDetailPage();
+        assertEquals(expected,actual);
+    }
+}
